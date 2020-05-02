fbf9a893640340bf9e393c5c059afa6e434d4b22
Felix Jian <felix.d.jian@gmail.com>
2020-05-02 05:41:44 +0200

    automate build and run with docker compose

---
+++ README.md
@@ -0,0 +1,9 @@
+# Engagement Detection Infrastructure
+
+Docker image of `activity-detector` necessary, source and Dockerfile will be added as a git submodule later.
+
+All other services can be built and run by using:
+
+```
+docker-compose up -d --build
+```
---
+++ connector-creator/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# TODO: replace hardcoded values with environment variables
+
+ERR='Error: Missing $PARAM parameter'
+
+[ -z "$CONNECT_HOST" ] && CONNECT_HOST="$1" && 
+    [ -z "$CONNECT_HOST" ] && { echo $ERR | PARAM="host" envsubst; exit 1; }
+
+[ -z "$CONNECT_PORT" ] && CONNECT_PORT="$2" && 
+    [ -z "$CONNECT_PORT" ] && { echo $ERR | PARAM="port" envsubst; exit 1; }
+
+echo "Waiting for rest endpoint at $CONNECT_HOST:$CONNECT_PORT"
+
+until RE="$(curl -sf -m 1 "http://$CONNECT_HOST:$CONNECT_PORT/connectors")"; do
+    printf "."
+    sleep 2
+done
+echo
+
+JSON_CONFIG=/data/config.json
+CONNECT_NAME="$(jq -r '.name' $JSON_CONFIG)"
+
+! echo "$RE" | jq -e "index(\"$CONNECT_NAME\") and true or false" > /dev/null && {
+    echo "Connector '$CONNECT_NAME' not running. Creating it via REST API." &&
+    curl -sSX POST \
+    -H "Content-Type: application/json" \
+    --data-binary "@$JSON_CONFIG" \
+    "http://$CONNECT_HOST:$CONNECT_PORT/connectors" &&
+    printf "\nSuccesfully created Connector '$CONNECT_NAME'.\n" ||
+    echo "Error creating connector '$CONNECT_NAME'" ;
+} ||
+    echo "Connector '$CONNECT_NAME' is already running"
+
