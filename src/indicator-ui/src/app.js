0b8c4539b7b763e6bf11402a38246a57fb57b901
Felix Jian <felix.d.jian@gmail.com>
2020-07-27 12:46:29 +0200

    cleanup

--- src/indicator-ui/src/app.js
+++ src/indicator-ui/src/app.js
@@ -1,3 +1,5 @@
+// TODO: Fix stats chart
+
 const MAX_LENGTH_STATS = 20;
 
 const GYRO_CHART = 'gyro-chart';
@@ -74,54 +76,13 @@ let connected = false;
 let isRunning = false;
 
 window.onload = function() {
-  const btn = document.createElement("BUTTON");
-  btn.id = 'btn-connect'
-  btn.textContent = "CONNECT";
-  document.getElementById("buttons").appendChild(btn);
-  btn.disabled = true;
-  btn.onclick = function () {
-    console.log("connect btn")
-    connect();
-  }
-
-  const disc = document.createElement("BUTTON");
-  disc.id = 'btn-disconnect'
-  disc.textContent = "DISCONNECT";
-  disc.disabled = true;
-  document.getElementById("buttons").appendChild(disc);
-  disc.onclick = function () {
-    console.log("disconnect btn")
-    disconnect();
-  }
-
-  const pause = document.createElement("BUTTON");
-  pause.id = 'btn-pause'
-  pause.textContent = "PAUSE GRAPH";
-  pause.disabled = true;
-  document.getElementById("buttons").appendChild(pause);
-  pause.onclick = function () {
-    clearGraphIntervals();
-  }
-
-  const resume = document.createElement("BUTTON");
-  resume.id = 'btn-resume'
-  resume.textContent = "RESUME GRAPH";
-  resume.disabled = true;
-  document.getElementById("buttons").appendChild(resume);
-  resume.onclick = function () {
-    setGraphIntervals();
-  }
-
-  const linAccChartCtx = document.getElementById(LIN_ACC_CHART).getContext('2d');
-  const acceleroChartCtx = document.getElementById(ACCELERO_CHART).getContext('2d');
-  const gyroChartCtx = document.getElementById(GYRO_CHART).getContext('2d');
-  const lightChartCtx = document.getElementById(LIGHT_CHART).getContext('2d');
+  createButtons();
 
   chartsById = { 
-    [LIN_ACC_CHART]: new Chart(linAccChartCtx, createAcceleroChartCfg(LIN_ACC_CHART)),
-    [ACCELERO_CHART]: new Chart(acceleroChartCtx, createAcceleroChartCfg(ACCELERO_CHART)),
-    [GYRO_CHART]: new Chart(gyroChartCtx, createAcceleroChartCfg(GYRO_CHART)),
-    [LIGHT_CHART]: new Chart(lightChartCtx, createAcceleroChartCfg(LIGHT_CHART))
+    [LIN_ACC_CHART]: createSensorChart(LIN_ACC_CHART),
+    [ACCELERO_CHART]: createSensorChart(ACCELERO_CHART),
+    [GYRO_CHART]: createSensorChart(GYRO_CHART),
+    [LIGHT_CHART]: createSensorChart(LIGHT_CHART)
   };
     
   const statChartCtx = document.getElementById('stat-chart').getContext('2d');
@@ -156,50 +117,12 @@ function connect() {
     });
 
   });
+
   socket.onclose = function () {
     console.log("connection closed, reconnecting in 3s");
     setConnected(false);
     setTimeout(connect, 3000);
   }
-}
-
-function handleChartUpdate(record, chartId) {
-  const timings = chartData[chartId];
-  const now = Date.now();
-
-  if(now - timings.lastUpdated < 100) {
-    // use max 1 record every 100ms for performance reasons.
-    // the rest of the records will be ignored and not displayed
-    return 
-  }
-
-  timings.lastUpdated = now;
-  const chart = chartsById[chartId];
-
-  chart.data.datasets.forEach((dataset) => {
-    const curChartData = [record.x, record.y, record.z][getNum(dataset.label) - 23];
-    if (curChartData) {
-      dataset.data.push({ 
-        //x: record.time,
-        x: now,
-        y: curChartData
-      });
-    } else {
-      console.log("unknown dataset mentioned.")
-    }
-  });
-
-  chart.update({
-    preservation: true
-  });
-}
-
-function handleStats(stats) {
-  statsChartData.lastUpdated = new Date();
-  statsChartData.count = stats.count;
-  statsChartData.timeSum = stats.timeSumSec;
-  statsChartData.countPerSec = stats.countPerSecond;
-  statsChartData.stamp = stats.time;
 }
 
 function disconnect() {
@@ -209,109 +132,23 @@ function disconnect() {
   setConnected(false);
 }
 
-function setConnected(conn) {
-  connected = conn;
-  const btnCon = document.getElementById("btn-connect");
-  const btnDisc = document.getElementById("btn-disconnect");
-  const btnPs = document.getElementById("btn-pause");
-  const btnRes = document.getElementById("btn-resume");
 
-  if(conn) {
-    btnCon.disabled = true;
-    btnDisc.disabled = false;
-    refreshPauseBtns();
-  } else {
-    btnCon.disabled = false;
-    btnDisc.disabled = true;
-    btnPs.disabled = true;
-    btnRes.disabled = true;
-  }
+function createSensorChart(chartId) { 
+  return new Chart(
+    document.getElementById(chartId).getContext('2d'), 
+    createSensorChartCfg(chartId)); 
 }
 
-function refreshPauseBtns() {
-    const btnPs = document.getElementById("btn-pause");
-    const btnRes = document.getElementById("btn-resume");
-
-    if(!btnPs || !btnRes) {
-      return false;
-    }
-
-    if(isRunning) {
-      btnRes.disabled = true;
-      btnPs.disabled = false;
-    } else {
-      btnRes.disabled = false;
-      btnPs.disabled = true;
-    }
-}
-
-function clearGraphIntervals() {
-  if(!isRunning) {
-    return false;
-  }
-  isRunning = false;
-  // TODO: implement pause
-
-  refreshPauseBtns();
-  console.log("pause graph");
-}
-
-function setGraphIntervals() {
-  if(isRunning) {
-    return false;
-  }
-  isRunning = true;
-  // TODO: implement resume
-
-  refreshPauseBtns();
-  console.log("resume graph");
-}
-
-function toggleGraphIntervals() {
-  isRunning ? clearGraphIntervals() : setGraphIntervals();
-}
-
-function refreshStatsGraph() {
-  let now = new Date()
-  if (now - statsChartData.lastUpdated > 2000) {
-    statsChartData.countPerSec = "0";
-  }
-  statChart.data.labels.push(now);
-  if (statChart.data.labels.length > MAX_LENGTH_STATS) {
-    statChart.data.labels.shift();
-  }
-
-  statChart.data.datasets.forEach((dataset) => {
-    switch(dataset['label']) {
-      case 'countPerSec':
-        dataset.data.push(parseFloat(statsChartData.countPerSec));
-        break;
-      default:
-        console.log("unknown dataset mentioned.")
-    }
-    if (dataset.data.length > MAX_LENGTH_STATS ) {
-      dataset.data.shift();
-    }
-  });
-  statChart.update(0);
-}
-
-const indexToAlphaNumeric = i => ((num = i + 10) > 35 
-  ? num % 36 + 10 
-  : num).toString(36);
-const getNum = a => a.charCodeAt(0) - 97;
-
-function createAcceleroChartCfg(chartId) {
+function createSensorChartCfg(chartId) {
   // create as many datasets as the param length of the respective chart.
-  const datasets = [ ...Array(CHART_CONFIGS[chartId].paramLength).keys() ].map( (i) => 
-    ({
+  const datasets = [ ...Array(CHART_CONFIGS[chartId].paramLength).keys() ]
+    .map( (i) => ({
       // Array index is mapped to an alphanumeric parameter name alphabetically:
       // 0 => 'x', 1 => 'y', 2 => 'z', 3 => 'a', 4 => 'b', ... etc.
       label: indexToAlphaNumeric(i + 23),
       data: [],
       backgroundColor: CHART_COLORS[i % CHART_COLORS.length]
-      })
-  );
+    }));
 
   return {
     type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
@@ -379,5 +216,169 @@ function getStatsChartCfg() {
       }]
     }
   }
+}
+
+function handleChartUpdate(record, chartId) {
+  const timings = chartData[chartId];
+  const now = Date.now();
+
+  if(now - timings.lastUpdated < 100) {
+    // use max 1 record every 100ms for performance reasons.
+    // the rest of the records will be ignored and not displayed
+    return 
+  }
+
+  timings.lastUpdated = now;
+  const chart = chartsById[chartId];
+
+  chart.data.datasets.forEach((dataset) => {
+    const curChartData = [record.x, record.y, record.z][getNum(dataset.label) - 23];
+    if (curChartData) {
+      dataset.data.push({ 
+        //x: record.time,
+        x: now,
+        y: curChartData
+      });
+    } else {
+      console.log("unknown dataset mentioned.")
+    }
+  });
+
+  chart.update({
+    preservation: true
+  });
+}
+
+function handleStats(stats) {
+  statsChartData.lastUpdated = new Date();
+  statsChartData.count = stats.count;
+  statsChartData.timeSum = stats.timeSumSec;
+  statsChartData.countPerSec = stats.countPerSecond;
+  statsChartData.stamp = stats.time;
+}
+
+function toggleGraphIntervals() {
+  isRunning ? clearGraphIntervals() : setGraphIntervals();
+}
+
+function refreshStatsGraph() {
+  let now = new Date()
+  if (now - statsChartData.lastUpdated > 2000) {
+    statsChartData.countPerSec = "0";
+  }
+  statChart.data.labels.push(now);
+  if (statChart.data.labels.length > MAX_LENGTH_STATS) {
+    statChart.data.labels.shift();
+  }
+
+  statChart.data.datasets.forEach((dataset) => {
+    switch(dataset['label']) {
+      case 'countPerSec':
+        dataset.data.push(parseFloat(statsChartData.countPerSec));
+        break;
+      default:
+        console.log("unknown dataset mentioned.")
+    }
+    if (dataset.data.length > MAX_LENGTH_STATS ) {
+      dataset.data.shift();
+    }
+  });
+  statChart.update(0);
+}
+
+function setGraphIntervals() {
+  if(isRunning) {
+    return false;
+  }
+  isRunning = true;
+  // TODO: implement resume
+
+  refreshPauseBtns();
+  console.log("resume graph");
+}
+
+function clearGraphIntervals() {
+  if(!isRunning) {
+    return false;
+  }
+  isRunning = false;
+  // TODO: implement pause
+
+  refreshPauseBtns();
+  console.log("pause graph");
+}
+
+function setConnected(conn) {
+  connected = conn;
+  const btnCon = document.getElementById("btn-connect");
+  const btnDisc = document.getElementById("btn-disconnect");
+  const btnPs = document.getElementById("btn-pause");
+  const btnRes = document.getElementById("btn-resume");
+
+  if(conn) {
+    btnCon.disabled = true;
+    btnDisc.disabled = false;
+    refreshPauseBtns();
+  } else {
+    btnCon.disabled = false;
+    btnDisc.disabled = true;
+    btnPs.disabled = true;
+    btnRes.disabled = true;
+  }
+}
+
+function createButtons() {
+  createButton('btn-connect', 'CONNECT', true, function () {
+    console.log("connect btn")
+    connect();
+  });
+
+  createButton('btn-disconnect', 'DISCONNECT', true, function () {
+    console.log("disconnect btn")
+    disconnect();
+  })
+
+  createButton('btn-pause', 'PAUSE GRAPH', true, function () {
+    clearGraphIntervals();
+  });
+
+  createButton('btn-resume', 'RESUME GRAPH', true, function () {
+    setGraphIntervals();
+  });
+}
+
+function createButton(id, textContent, disabled, callback) {
+  const btn = document.createElement("BUTTON");
+  btn.id = id
+  btn.textContent = textContent;
+  document.getElementById("buttons").appendChild(btn);
+  btn.disabled = disabled;
+  btn.onclick = callback;
+  return btn;
+}
+
+function refreshPauseBtns() {
+    const btnPs = document.getElementById("btn-pause");
+    const btnRes = document.getElementById("btn-resume");
+
+    if(!btnPs || !btnRes) {
+      return false;
+    }
+
+    if(isRunning) {
+      btnRes.disabled = true;
+      btnPs.disabled = false;
+    } else {
+      btnRes.disabled = false;
+      btnPs.disabled = true;
+    }
+}
+
+function indexToAlphaNumeric(i) { 
+  return ((num = i + 10) > 35 ? num % 36 + 10 : num).toString(36);
+}
+
+function getNum(a) { 
+  return a.charCodeAt(0) - 97;
 }
 
