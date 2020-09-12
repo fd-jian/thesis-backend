958f57748bfc1b8db96e57f1ee01f26f59ddf14f
Felix Jian <felix.d.jian@gmail.com>
2020-09-12 10:06:26 +0200

    update scripts to show and push schemas

--- push_schema.sh
+++ push_schema.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-## Simple script to push one or multiple files or all files
-## in all subdirectories to the local schema registry with
-## default port recursively.
+## Push one or multiple files or all files in all subdirectories to the local
+## schema registry with default port recursively.
+## 
+## If URL is not specified, a hardcoded default URL will be used and the
+## remaining arguments will be seen as a list of files and/or directories.
 ##
-## If URL is not specified, a hardcoded default URL will
-## be used.
-##
-## Usage: push_schema.sh [URL|-] [FILE|DIRECTORY]...
+## To omit the URL argument, use `-` followed by the other argument.
+## 
+## Usage: ./push_schema.sh [URL|-] [FILE|DIRECTORY]...
 
 SUBJECTS_URL="$1"
 shift
@@ -30,12 +31,15 @@ push_file() {
     echo "Missing argument" >&2 &&
     exit 1
 
+# iterate all arguments. pathes with whitespaces need double quotes
 for FILEPATH in "$@"; do
+    # if argument is a directory, search subdirs for .avsc files to push
     if test -d "$FILEPATH"; then
         find "$FILEPATH" -type f -name "*.avsc" |
             while read FILE; do
                 push_file "$FILE"
             done
+    # if arugment is a file, push the file
     elif test -f "$FILEPATH"; then
         push_file "$FILEPATH"
     else
--- show_schema.sh
+++ show_schema.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
 
-curl -sfS http://${3:-'localhost:8081'}/subjects/$1/versions/${2:-'1'} \
-    | jq --raw-output '.schema' | jq
+## Retrieve schema information from the registry. If no arguments are
+## specified, A list of all subjects will be queried from a hardcoded URL. 
+
+## If URL and subject are provided, the list of versions for the subject will
+## be queried. If URL, subject, and version are specified, the specific version
+## of the subject will be retrieved. 
+
+## To omit the URL argument, use `-` followed by the other argument.
+
+## Usage: ./show_schema.sh [URL|-] [SUBJECT] [VERSION]
+
+URL="$1"
+[ "$URL" = "-" ] && URL='http://localhost:8081'
+URL=$URL/subjects
+
+JQ_STR=
+! test -z "$2" && {
+    URL=$URL/$2/versions
+    ! test -z "$3" && {
+        URL=$URL/$3 
+        JQ_STR='.schema'
+    }
+}
+
+curl -sfS $URL \
+    | jq --raw-output "$JQ_STR" | jq
